875cf04fb48edca73180e6b64c8cba9439eaeb78
Sherex <ingar@helgesn.com>
2019-01-31 14:54:57 +0100

    Terminology update

--- Relative-parser-http-trigger/index.js
+++ Relative-parser-http-trigger/index.js
@@ -3,9 +3,8 @@ function base64ToObj (base64) {
 }
 
 function parseMinElevData (data) {
-  var jsonData = base64ToObj(data)
   try {
-    var recipientsMail = jsonData.kontaktpersonData
+    var recipientsMail = data.kontaktpersonData
       .map(contactPerson => contactPerson.epost)
       .filter(mail => !(mail === ''))
   } catch (error) {
@@ -14,16 +13,16 @@ function parseMinElevData (data) {
   }
 
   return {
-    sender: jsonData.userMail,
+    sender: data.userMail,
     recipients: recipientsMail,
-    document: jsonData.document
+    document: data.document
   }
 }
 
 module.exports = async function (context, req) {
   var response
   if (req.body && req.body.ContentData) {
-    response = parseMinElevData(req.body.ContentData)
+    response = parseMinElevData(base64ToObj(req.body.ContentData))
   } else {
     response = parseMinElevData(req.body)
   }

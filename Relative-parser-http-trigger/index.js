6fa4c91c2f2d7aea5e78a645a0be12d5f65b301b
Sherex <ingar@helgesn.com>
2019-02-25 14:06:24 +0100

    Returning teacher name; Using context for logging

--- Relative-parser-http-trigger/index.js
+++ Relative-parser-http-trigger/index.js
@@ -2,17 +2,18 @@ function base64ToObj (base64) {
   return JSON.parse(Buffer.from(base64, 'base64').toString('utf-8'))
 }
 
-function parseMinElevData (data) {
+function parseMinElevData (context, data) {
   try {
     var recipientsMail = data.kontaktpersonData
       .map(contactPerson => contactPerson.epost)
       .filter(mail => !(mail === ''))
   } catch (error) {
-    console.error('Error in parsing of data. Message: \n', error)
+    context.log('Error in parsing of data. Message: \n', error)
     return
   }
 
   return {
+    teacher: data.userName,
     sender: data.userMail,
     recipients: recipientsMail,
     document: data.document
@@ -22,9 +23,9 @@ function parseMinElevData (data) {
 module.exports = async function (context, req) {
   var response
   if (req.body && req.body.ContentData) {
-    response = parseMinElevData(base64ToObj(req.body.ContentData))
+    response = parseMinElevData(context, base64ToObj(req.body.ContentData))
   } else {
-    response = parseMinElevData(req.body)
+    response = parseMinElevData(context, req.body)
   }
 
   if (response) {
